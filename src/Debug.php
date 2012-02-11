f0b37ec98b63cf29d719bfd2588d572969bc7802
Marlin Cremers <marlinc@mms-projects.net>
2012-02-11 17:57:02 +0100

    Created a console color class

--- src/Debug.php
+++ src/Debug.php
@@ -7,7 +7,17 @@ class Debug extends \IRCBot_Debugger_Abstract
 
     public function log($category, $type, $message,
         $level = IRCBOT_DEBUG_NORMAL) {
-        echo sprintf('[%s|%s] %s', $category, $type, $message) . PHP_EOL;   
+        $C = new Console\Colors;
+        echo $this->getSign($category, $type)
+            . ' ' . $C(1, $message)
+            . PHP_EOL;   
+    }
+    
+    public function getSign($category, $type)
+    {
+        $C = new Console\Colors;
+        return $C(1) . '[' . $C(4, $category) . $C(1) . '|'
+            . $C(4, $type) . $C(1) . ']' . $C();
     }
 
 }
