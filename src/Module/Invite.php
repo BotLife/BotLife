ac47ba7bf6ebef03281b5312a9a2b747d976485e
Marlin Cremers <marlinc@mms-projects.net>
2012-02-20 19:12:23 +0100

    BotLife now rejoins a channel if quited

--- src/Module/Invite.php
+++ src/Module/Invite.php
@@ -8,13 +8,34 @@ class Invite extends AModule
     public $events = array(
         'onInvite',
         'channelReady',
+        'onConnect'      => 'joinChannels',
     );
+
+    public function __construct()
+    {
+        $invites = \Botlife\Application\Storage::loadData('invites');
+        if (!isset($invites->channels)) {
+            $invites->channels = array();
+        }
+        \Botlife\Application\Storage::saveData('invites', $invites);
+        parent::__construct();
+    }
+
+    public function joinChannels()
+    {
+        $invites = \Botlife\Application\Storage::loadData('invites');
+        if (empty($invites->channels)) {
+            return;
+        }
+        $channels = implode(',', array_keys($invites->channels));
+        \Ircbot\joinChan($channels);
+    }
 
     public function onInvite($event)
     {
         $invites = \Botlife\Application\Storage::loadData('invites');
         $hash    = md5($event->channel); 
-        $invites->$hash = $event;
+        $invites->tmp->$hash = $event;
         \Ircbot\joinChan($event->channel);
         \Botlife\Application\Storage::saveData('invites', $invites);
     }
@@ -24,10 +45,10 @@ class Invite extends AModule
         $c       = new \Botlife\Application\Colors;
         $invites = \Botlife\Application\Storage::loadData('invites');
         $hash    = md5(\Ircbot\token('0'));
-        if (!isset($invites->$hash)) {
+        if (!isset($invites->tmp->$hash)) {
             return;
         }
-        $invite  = $invites->$hash;
+        $invite  = $invites->tmp->$hash;
         $channel = \Ircbot\Application::getInstance()->getChannelHandler()
             ->getChan(\Ircbot\token('0'), $event->botId);
         if (!$channel->isOp($invite->mask->nickname)) {
@@ -37,6 +58,11 @@ class Invite extends AModule
             );
             return;
         } 
+        unset($invites->tmp->$hash);
+        $channel = new \StdClass;
+        $channel->autojoin = true;
+        $invites->channels[\Ircbot\token('0')] = $channel;
+        \Botlife\Application\Storage::saveData('invites', $invites);
         \Ircbot\Application::getInstance()->getEventHandler()
             ->raiseEvent(
                 'inviteSucceed', array(\Ircbot\token('0'), $invite)
